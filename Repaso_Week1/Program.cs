a18c81eafe11862398c53ea8b749fe186acca753
SamuelLeiva <leiva20152@gmail.com>
2025-04-04 20:22:23 -0500

    Agregar archivos de proyecto.

---
+++ Repaso_Week1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Repaso_Week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor a comprobar: ");
+            int numero = Convert.ToInt32 (Console.ReadLine());
+
+            if (numero % 2 == 0) 
+            {
+                Console.WriteLine("El número es par");
+            } 
+            else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+        }
+    }
+}
