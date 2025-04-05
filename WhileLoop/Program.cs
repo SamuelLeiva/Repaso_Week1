75d284fa3fbdccd66a7ef9a38c3994949c8dd756
SamuelLeiva <leiva20152@gmail.com>
2025-04-04 22:53:05 -0500

    Pattern added

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -5,19 +5,45 @@
         static void Main(string[] args)
         {
             //Tabla de multiplicar
-            int valorTabla;
-            int contador = 1;
-            int resultado;
-            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
-            valorTabla = Convert.ToInt32(Console.ReadLine());
+            //int valorTabla;
+            //int contador = 1;
+            //int resultado;
+            //Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            //valorTabla = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Tabla del " + valorTabla);
+            //Console.WriteLine("Tabla del " + valorTabla);
 
-            while(contador <= 12)
+            //while(contador <= 12)
+            //{
+            //    resultado = valorTabla * contador;
+            //    Console.WriteLine(valorTabla + " X " + contador + " = " + resultado);
+            //    contador++;
+            //}
+
+            //cuadrado de 1 al 5
+
+            Console.WriteLine("Ingrese un número");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int inicio = 1; //numero de reglón
+            
+
+            while(inicio <= num)
             {
-                resultado = valorTabla * contador;
-                Console.WriteLine(valorTabla + " X " + contador + " = " + resultado);
-                contador++;
+                int cont = 1; //numero de reglón
+                while (cont <= num)
+                {
+                    if( inicio >= cont)
+                    {
+                        Console.Write(cont + " ");
+                    } else
+                    {
+                        Console.Write("* ");
+                    }
+                        
+                    cont++;
+                }
+                inicio++;
+                Console.Write("\n");
             }
         }
     }
