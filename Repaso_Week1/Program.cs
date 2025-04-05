4c7e03e8c56523a664cf414eaf92a411f35aa476
SamuelLeiva <leiva20152@gmail.com>
2025-04-04 20:49:54 -0500

    Exercise 2 added

--- Repaso_Week1/Program.cs
+++ Repaso_Week1/Program.cs
@@ -15,6 +15,32 @@
             {
                 Console.WriteLine("El numero es impar");
             }
+
+            Console.WriteLine("Cuál es el numero mayor de 3 números ");
+            Console.WriteLine("Ingrese el primer número");
+            int primerNumero = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int segundoNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número");
+            int tercerNumero = Convert.ToInt32(Console.ReadLine());
+
+            if(primerNumero > segundoNumero && primerNumero > tercerNumero)
+            {
+                Console.WriteLine(primerNumero + " es el número mayor");
+            }
+            else if (segundoNumero > primerNumero && segundoNumero > tercerNumero)
+            {
+                Console.WriteLine(segundoNumero + " es el número mayor");
+            }
+            else if (tercerNumero > primerNumero && tercerNumero > segundoNumero)
+            {
+                Console.WriteLine(tercerNumero + " es el número mayor");
+            }
+            else
+            {
+                Console.WriteLine("Todos los números son iguales");
+            }
+          
         }
     }
 }
