01a954221bc5cdbd62b593266b9c0127d79f826c
SamuelLeiva <leiva20152@gmail.com>
2025-04-04 22:04:43 -0500

    Added WhileLoop Project

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tabla de multiplicar
+            int valorTabla;
+            int contador = 1;
+            int resultado;
+            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            valorTabla = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tabla del " + valorTabla);
+
+            while(contador <= 12)
+            {
+                resultado = valorTabla * contador;
+                Console.WriteLine(valorTabla + " X " + contador + " = " + resultado);
+                contador++;
+            }
+        }
+    }
+}
