d5677137d9e4f1d2b547799319bc84d0fa49e676
SamuelLeiva <leiva20152@gmail.com>
2025-04-06 18:26:37 -0500

    Added ForLoop and For homework

---
+++ ForLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Hacer tabla de multiplicar con for
+            int valorTabla;
+            int resultado;
+            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            valorTabla = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tabla del " + valorTabla);
+
+            for (int i = 1; i <= 12; i++)
+            {
+                resultado = valorTabla * i;
+                Console.WriteLine(valorTabla + " X " + i + " = " + resultado);
+            }
+
+            //2. Hacer el patrón de num y * con for
+            Console.WriteLine("Ingrese el valor para el patrón");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            for (int j = 1; j <= valor; j++) //reglón
+            {   
+                for (int k = 1; k <= valor; k++) //caracter
+                {
+                    if (j >= k)
+                    {
+                        Console.Write(k + " ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -22,29 +22,47 @@
 
             //cuadrado de 1 al 5
 
-            Console.WriteLine("Ingrese un número");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int inicio = 1; //numero de reglón
+            //Console.WriteLine("Ingrese un número");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int inicio = 1; //numero de reglón
             
 
-            while(inicio <= num)
-            {
-                int cont = 1; //numero de reglón
-                while (cont <= num)
-                {
-                    if( inicio >= cont)
-                    {
-                        Console.Write(cont + " ");
-                    } else
-                    {
-                        Console.Write("* ");
-                    }
+            //while(inicio <= num)
+            //{
+            //    int cont = 1; //numero de reglón
+            //    while (cont <= num)
+            //    {
+            //        if( inicio >= cont)
+            //        {
+            //            Console.Write(cont + " ");
+            //        } else
+            //        {
+            //            Console.Write("* ");
+            //        }
                         
-                    cont++;
-                }
-                inicio++;
-                Console.Write("\n");
+            //        cont++;
+            //    }
+            //    inicio++;
+            //    Console.Write("\n");
+            //}
+
+            //1. Hacer tabla de multiplicar con for
+            int valorTabla;
+            int resultado;
+            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            valorTabla = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tabla del " + valorTabla);
+
+            for(int i = 1; i <= 12; i++)
+            {
+                resultado = valorTabla * i;
+                Console.WriteLine(valorTabla + " X " + i + " = " + resultado);
             }
+
+            
+
+            //2. Hacer el patrón de num y * con for
         }
     }
 }
