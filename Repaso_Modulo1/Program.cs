027b506a27d50ccefb086b576cf433bdec522eec
SamuelLeiva <leiva20152@gmail.com>
2025-04-11 18:28:01 -0500

    Duplicate numbers exercise added

---
+++ Repaso_Modulo1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Repaso_Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios de los arreglos
+            //contar los numeros que se repiten dentro de un arreglo
+            int[] array = { 44, 44, 44, 23, 23, 23, 23, 25, 25, 30, 29, 25, 80 };
+
+            HashSet<int> onlyNumbers = new HashSet<int>();
+
+            HashSet<int> duplicatesSet = new HashSet<int>();
+
+
+            foreach (int number in array)
+            {
+                if (onlyNumbers.Contains(number))
+                {
+                    duplicatesSet.Add(number);
+                } else
+                {
+                    onlyNumbers.Add(number);
+                } 
+            }
+
+            //mostrar resultados repetidos
+            Console.WriteLine("Resultados repetidos:");
+            foreach (int number in duplicatesSet)
+            {
+                Console.Write($"- {number} -");
+            }
+
+
+
+            
+        }
+    }
+}
