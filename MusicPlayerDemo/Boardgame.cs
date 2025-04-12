3e7498d3f090d6869d1ccf95bc6d938ef4c99cde
SamuelLeiva <leiva20152@gmail.com>
2025-04-11 22:19:11 -0500

    Boardgame class created

---
+++ MusicPlayerDemo/Boardgame.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerDemo
+{
+    public class Boardgame
+    {
+        private string Title;
+        private string Description;
+        private string Author;
+        private string Duration;
+        private double Grade;
+
+        public Boardgame(string title, string description,  string author, string duration, double grade)
+        {
+            Title = title; 
+            Description = description;
+            Author = author; 
+            Duration = duration;
+            Grade = grade;
+        }
+
+        public string getTitle()
+        {
+            return Title;
+        }
+
+        public void setTitle(string title)
+        {
+            this.Title = title;
+        }
+
+        public string getDescription()
+        {
+            return Description;
+        }
+
+        public void setDescription(string description)
+        {
+            this.Description = description;
+        }
+
+        public string getAuthor()
+        {
+            return Author;
+        }
+
+        public void setAuthor(string author)
+        {
+            this.Author = author;
+        }
+
+        public string getDuration()
+        {
+            return this.Duration;
+        }
+
+        public void setDuration(string duration)
+        {
+            this.Duration = duration;
+        }
+
+        public double getGrade()
+        {
+            return Grade;
+        }
+
+        public void setGrade(double grade)
+        {
+            if(grade < 0 || grade > 10)
+            {
+                throw new ArgumentException("Nota inválida.");
+            }
+            Grade = grade;
+        }
+    }
+}
---
+++ MusicPlayerDemo/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerDemo
+{
+    public class Song
+    {
+        public string Album {  get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+
+        public Song (string album, string title, string description, string genre)
+        {
+            Album = album;
+            Title = title;
+            Description = description;
+            Genre = genre;
+        }
+    }
+}
