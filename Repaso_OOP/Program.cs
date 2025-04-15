b0a7a87d030b718fbc80cdc738616de71927f8e6
SamuelLeiva <leiva20152@gmail.com>
2025-04-15 15:59:03 -0500

    Repaso_OOP project added

---
+++ Repaso_OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Dynamic;
+
+namespace Repaso_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Song()
+        {
+            public string name;
+            public string artist;
+            public string genre;
+            public string albumName;
+            public int durationSeconds;
+
+            public Song(string name, string artist, string genre, string albumName, int durationSeconds) : this()
+            {
+                this.name = name;
+                this.artist = artist;
+                this.genre = genre;
+                this.albumName = albumName;
+                this.durationSeconds = durationSeconds;
+            }
+        }
+    }
+}
